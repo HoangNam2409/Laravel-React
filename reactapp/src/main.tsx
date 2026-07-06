59e1e87e60e7e7378a45c25fcf131dd8d051e8e3
HoangNam2409 <namzinv123@gmail.com>
2026-07-06 23:57:53 +0700

    Cài đặt dự án, tạo Login Page

---
+++ reactapp/src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/admin",
+    element: <Login/>,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>,
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
